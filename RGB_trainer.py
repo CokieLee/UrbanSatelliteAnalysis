2ecad30107eee3da66427abee58fa36a7daa06e4
Jacob Sorensen <sorensenjacob44@gmail.com>
2025-10-16 16:15:20 -0400

    Add model evaluation and accuracy output csv

--- RGB_trainer.py
+++ RGB_trainer.py
@@ -57,23 +57,106 @@ dataloader = DataLoader(
     pin_memory=True  # Faster data transfer to GPU (if using GPU)
 )
 
+def Do_Training(model,num_epochs,save_interval,save_dir):
+        
+    torch.save(model.state_dict(), os.path.join(save_dir, 'model_init.pth'))
+
+    for epoch in range(num_epochs):
+        # Iterate over training batches
+        print(f"Epoch [{epoch + 1}/{num_epochs}]")
+
+        for images, labels in dataloader:
+            # Move data to device
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            scores = model(images)  # Shape: [batch_size, num_classes]
+            loss = criterion(scores, labels)  # labels should be shape [batch_size]
+
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        if (epoch) % save_interval == 0:
+            torch.save(model.state_dict(), os.path.join(save_dir, f'epoch_{epoch+1}.pth'))
+    torch.save(model.state_dict(), os.path.join(save_dir, 'model_fin.pth'))
+
+
+import math
+def Training_Is_Done(num_epochs,save_interval,saved_model_states):
+    if os.path.isdir(saved_model_states):
+        save_names = [os.path.basename(f) for f in os.listdir(saved_model_states)]
+        if len(save_names) >= math.ceil(num_epochs / save_interval) + 2:
+            return True
+    return False
+
+
+import os
 num_epochs=10
-for epoch in range(num_epochs):
-    # Iterate over training batches
-    print(f"Epoch [{epoch + 1}/{num_epochs}]")
+save_interval = 1
+saved_model_states = "Basic_RGB"
 
-    for images, labels in dataloader:
-        # Move data to device
-        images = images.to(device)
-        labels = labels.to(device)
+# need to determine if the training has already happened, 
+# otherwise we can just go straight to model analysis
 
-        # Forward pass
-        scores = model(images)  # Shape: [batch_size, num_classes]
-        loss = criterion(scores, labels)  # labels should be shape [batch_size]
+if not Training_Is_Done(num_epochs,save_interval,saved_model_states):
+    Do_Training(model,num_epochs,save_interval,saved_model_states)
 
-        # Backward pass and optimization
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+print("Training complete!!!")
 
-torch.save(model.state_dict(), 'RGB_model_state.pth')
+# Lets evaluate the results
+
+import polars as pl
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=batch_size,
+    shuffle=True,  # Shuffle for training
+    num_workers=4,  # Parallel data loading (adjust based on CPU cores)
+    pin_memory=True  # Faster data transfer to GPU (if using GPU)
+)
+
+
+save_names = [os.path.join(saved_model_states,f) for f in os.listdir(saved_model_states)]
+results_df = pl.DataFrame({
+    "Category" : class_names
+})
+for path in save_names:
+    net = Basic_CNN(3,10)
+    net.load_state_dict(torch.load(path, weights_only=True))
+
+    correct_pred = {classname: 0 for classname in class_names}
+    total_pred = {classname: 0 for classname in class_names}
+
+    # again no gradients needed
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predictions = torch.max(outputs, 1)
+            # collect the correct predictions for each class
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[class_names[label]] += 1
+                total_pred[class_names[label]] += 1
+    # pl.DataFrame()
+    # results_df.insert_column(os.path.basename(path),)
+    print()
+    model_accuracy = {}
+    # print accuracy for each class
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        model_accuracy[classname] = accuracy
+        # print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+    # print(f"model:{os.path.basename(path)}",model_accuracy)
+
+    model_df = pl.DataFrame({
+        "Category": list(model_accuracy.keys()),
+        os.path.basename(path): list(model_accuracy.values())
+    })
+    results_df = results_df.join(model_df, on="Category", how="left")
+
+results_df.write_csv(saved_model_states + '_accuracies.csv')
+
