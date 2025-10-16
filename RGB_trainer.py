2ebf8e5afe882d29c88504537889d8dae6a086b5
Jacob Sorensen <sorensenjacob44@gmail.com>
2025-10-16 11:14:44 -0400

    Move CNN class to separate file and renamed it

--- RGB_trainer.py
+++ RGB_trainer.py
@@ -1,15 +1,11 @@
 import torch
 from torch import nn
 from torch import optim
-import torch.nn.functional as F
 from torchvision.datasets import ImageFolder
 from torch.utils.data import Dataset, DataLoader, Subset
 
 from torchvision import transforms
 
-import os
-import numpy as np
-from PIL import Image
 from sklearn.model_selection import train_test_split
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -41,50 +37,9 @@ train_idx, test_idx = train_test_split(
 train_dataset = Subset(full_dataset, train_idx)
 test_dataset = Subset(full_dataset, test_idx)
 
-class CNN(nn.Module):
-    def __init__(self, in_channels, num_classes):
+from image_models import Basic_CNN
 
-        """
-        Building blocks of convolutional neural network.
-
-        Parameters:
-            * in_channels: Number of channels in the input image (for grayscale images, 1)
-            * num_classes: Number of classes to predict. In our problem, 10 (i.e digits from  0 to 9).
-        """
-        super(CNN, self).__init__()
-
-        # 1st convolutional layer
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=3, padding=1)
-        # Max pooling layer
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        # 2nd convolutional layer
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=1, kernel_size=3, padding=1)
-        # Fully connected layer
-        self.fc1 = nn.Linear(16 * 16, num_classes)
-
-
-    def forward(self, x):
-        """
-        Define the forward pass of the neural network.
-
-        Parameters:
-            x: Input tensor.
-
-        Returns:
-            torch.Tensor
-                The output tensor after passing through the network.
-        """
-        x = F.relu(self.conv1(x))  # Apply first convolution and ReLU activation
-        x = self.pool(x)           # Apply max pooling
-        x = F.relu(self.conv2(x))  # Apply second convolution and ReLU activation
-        x = self.pool(x)           # Apply max pooling
-        x = x.reshape(x.shape[0], -1)  # Flatten the tensor
-        x = self.fc1(x)            # Apply fully connected layer
-        x = torch.nn.functional.softmax(x,dim=1) # apply softmax to x
-        # x = torch.nn.functional.max()
-        return x
-
-model = CNN(in_channels=3, num_classes=10).to(device)
+model = Basic_CNN(3,10).to(device)
 
 # Define the loss function
 criterion = nn.CrossEntropyLoss()
