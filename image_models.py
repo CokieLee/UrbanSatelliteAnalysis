c95370d1089c8da72cedc7103ba0d5759cedadb7
Jacob Sorensen <sorensenjacob44@gmail.com>
2025-10-18 19:01:53 -0400

    Add Deepest_CNN and it intermediates from Basic_CNN. This model has 2 additional convolutional layers compared to Basic, employs batch Normalization in-between layers, and employs dropout during training.

--- image_models.py
+++ image_models.py
@@ -45,6 +45,277 @@ class Basic_CNN(nn.Module):
         # x = torch.nn.functional.max()
         return x
 
+class Deep_CNN(nn.Module):
+    def __init__(self, in_channels, num_classes):
+
+        """
+        Building blocks of convolutional neural network.
+
+        Parameters:
+            * in_channels: Number of channels in the input image (for grayscale images, 1)
+            * num_classes: Number of classes to predict. In our problem, 10 (i.e digits from  0 to 9).
+        """
+        super(Deep_CNN, self).__init__()
+
+        # convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=3, padding=1)
+        
+        # Max pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layer
+        self.fc1 = nn.Linear(16 * 16, num_classes)
+
+
+    def forward(self, x):
+        """
+        Define the forward pass of the neural network.
+
+        Parameters:
+            x: Input tensor.
+
+        Returns:
+            torch.Tensor
+                The output tensor after passing through the network.
+        """
+        x = F.relu(self.conv1(x))  
+        x = self.pool(x)           
+        x = F.relu(self.conv2(x))  
+        x = self.pool(x)           
+        x = F.relu(self.conv3(x))  
+        x = self.pool(x)           
+        x = x.reshape(x.shape[0], -1)  # Flatten the tensor
+        x = self.fc1(x)            # Apply fully connected layer
+        x = torch.nn.functional.softmax(x,dim=1) # apply softmax to x
+        # x = torch.nn.functional.max()
+        return x
+
+class Deep_CNN_Dropout(nn.Module):
+    def __init__(self, in_channels, num_classes):
+
+        """
+        Building blocks of convolutional neural network.
+
+        Parameters:
+            * in_channels: Number of channels in the input image (for grayscale images, 1)
+            * num_classes: Number of classes to predict. In our problem, 10 (i.e digits from  0 to 9).
+        """
+        super(Deep_CNN_Dropout, self).__init__()
+
+        # convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=3, padding=1)
+        
+        # Max pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layer
+        self.fc1 = nn.Linear(16 * 16, num_classes)
+
+        # Add Dropout
+        self.dropout = nn.Dropout(p=0.5)  # 50% dropout rate
+
+
+    def forward(self, x):
+        """
+        Define the forward pass of the neural network.
+
+        Parameters:
+            x: Input tensor.
+
+        Returns:
+            torch.Tensor
+                The output tensor after passing through the network.
+        """
+        x = F.relu(self.conv1(x))  
+        x = self.pool(x)           
+        x = F.relu(self.conv2(x))  
+        x = self.pool(x)           
+        x = F.relu(self.conv3(x))  
+        x = self.pool(x)           
+        x = x.reshape(x.shape[0], -1)  # Flatten the tensor
+        x = self.dropout(x)  # Apply dropout before fully connected layer
+        x = self.fc1(x)            # Apply fully connected layer
+        x = torch.nn.functional.softmax(x,dim=1) # apply softmax to x
+        # x = torch.nn.functional.max()
+        return x
+
+
+class Deep_CNN_Dropout_BatchNorm(nn.Module):
+    def __init__(self, in_channels, num_classes):
+
+        """
+        Building blocks of convolutional neural network.
+
+        Parameters:
+            * in_channels: Number of channels in the input image (for grayscale images, 1)
+            * num_classes: Number of classes to predict. In our problem, 10 (i.e digits from  0 to 9).
+        """
+        super(Deep_CNN_Dropout_BatchNorm, self).__init__()
+
+        # convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=3, padding=1)
+
+        self.bn1 = nn.BatchNorm2d(8)  # BatchNorm for conv1 output
+        self.bn2 = nn.BatchNorm2d(8)  # BatchNorm for conv2 output
+        self.bn3 = nn.BatchNorm2d(4)  # BatchNorm for conv3 output
+        
+        # Max pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layer
+        self.fc1 = nn.Linear(16 * 16, num_classes)
+
+        # Add Dropout
+        self.dropout = nn.Dropout(p=0.5)  # 50% dropout rate
+
+
+    def forward(self, x):
+        """
+        Define the forward pass of the neural network.
+
+        Parameters:
+            x: Input tensor.
+
+        Returns:
+            torch.Tensor
+                The output tensor after passing through the network.
+        """
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)  # Flatten the tensor
+        x = self.dropout(x)  # Apply dropout before fully connected layer
+        x = self.fc1(x)            # Apply fully connected layer
+        x = torch.nn.functional.softmax(x,dim=1) # apply softmax to x
+        # x = torch.nn.functional.max()
+        return x
+
+
+class Deep_CNN_Dropout_BatchNorm_No_Softmax(nn.Module):
+    def __init__(self, in_channels, num_classes):
+
+        """
+        Building blocks of convolutional neural network.
+
+        Parameters:
+            * in_channels: Number of channels in the input image (for grayscale images, 1)
+            * num_classes: Number of classes to predict. In our problem, 10 (i.e digits from  0 to 9).
+        """
+        super(Deep_CNN_Dropout_BatchNorm_No_Softmax, self).__init__()
+
+        # convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=3, padding=1)
+
+        self.bn1 = nn.BatchNorm2d(8)  # BatchNorm for conv1 output
+        self.bn2 = nn.BatchNorm2d(8)  # BatchNorm for conv2 output
+        self.bn3 = nn.BatchNorm2d(4)  # BatchNorm for conv3 output
+        
+        # Max pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layer
+        self.fc1 = nn.Linear(16 * 16, num_classes)
+
+        # Add Dropout
+        self.dropout = nn.Dropout(p=0.5)  # 50% dropout rate
+
+
+    def forward(self, x):
+        """
+        Define the forward pass of the neural network.
+
+        Parameters:
+            x: Input tensor.
+
+        Returns:
+            torch.Tensor
+                The output tensor after passing through the network.
+        """
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)  # Flatten the tensor
+        x = self.dropout(x)  # Apply dropout before fully connected layer
+        x = self.fc1(x)            # Apply fully connected layer
+        return x
+
+class Deepest_CNN(nn.Module):
+    def __init__(self, in_channels, num_classes):
+
+        """
+        This model has 2 more layers compared to the original model
+
+        This model also includes batch dropout, batch normalization, 
+        and doesn't apply softmax to the final output.
+
+        It seems to perform quite well after 12 epochs.
+
+        It'll be interesting to see how it compares to some other peoples models! 
+
+        Parameters:
+            * in_channels: Number of channels in the input image (for grayscale images, 1)
+            * num_classes: Number of classes to predict. In our problem, 10 (i.e digits from  0 to 9).
+        """
+        super(Deepest_CNN, self).__init__()
+
+        # convolutional layers
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1)
+
+        self.bn1 = nn.BatchNorm2d(8)  # BatchNorm for conv1 output
+        self.bn2 = nn.BatchNorm2d(8)  # BatchNorm for conv2 output
+        self.bn3 = nn.BatchNorm2d(16)  # BatchNorm for conv3 output
+        self.bn4 = nn.BatchNorm2d(16)  # BatchNorm for conv4 output
+        
+        # Max pooling layer
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Fully connected layer
+        self.fc1 = nn.Linear(16 * 16, num_classes)
+
+        # Add Dropout
+        self.dropout = nn.Dropout(p=0.5)  # 50% dropout rate
+
+
+    def forward(self, x):
+        """
+        Define the forward pass of the neural network.
+
+        Parameters:
+            x: Input tensor.
+
+        Returns:
+            torch.Tensor
+                The output tensor after passing through the network.
+        """
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)  # Flatten the tensor
+        x = self.dropout(x)  # Apply dropout before fully connected layer
+        x = self.fc1(x)            # Apply fully connected layer
+        return x
 
 class transformer_CNN(nn.Module):
     def __init__(self):
