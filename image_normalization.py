eba421883ff563aba28a09e5527788d31794f749
Cokie Parker <cokie@parkertong.net>
2025-10-21 23:23:15 -0400

    added MS_trainer.py for training basic_CNN on multispectral data. Trained MS data on 40 epochs with Basic_CNN, .pth file stored in TrainedModels/MS_40epoch_Trained.pth

--- image_normalization.py
+++ image_normalization.py
@@ -2,14 +2,20 @@ import torch
 
 
 def get_mean_stdev(loader):
+    # get number of channels in images
+    dataiter = iter(loader)
+    first_batch_images, _ = next(dataiter)
+    first_image_tensor = first_batch_images[0]
+    _, num_channels, _, _ = first_batch_images.shape
+
     # Compute mean and stdev of all pixels in dataset
     num_images = 0
-    mean = torch.zeros(3)
-    var = torch.zeros(3)
+    mean = torch.zeros(num_channels)
+    var = torch.zeros(num_channels)
     num_pixels = 0
 
     for images, _ in loader:
-        batch_size, num_channels, height, width = images.shape
+        batch_size, _, height, width = images.shape
         num_pixels += batch_size * height * width
         mean += images.sum(dim=[0,2,3])
 
